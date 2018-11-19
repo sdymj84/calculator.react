76fb2fdc4971201488a346119753010aef1424be
Minjun Youn <minjun1.y@partner.samsung.com>
2018-11-19 11:41:07 -0600

    Updates

---
+++ precache-manifest.44e0cb9d4c01df354e352c1ab0e1caf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "54a96df13272341af0e9",
+    "url": "/calculator.react/static/js/runtime~main.54a96df1.js"
+  },
+  {
+    "revision": "c69d3c181a0020f3be99",
+    "url": "/calculator.react/static/js/main.c69d3c18.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/calculator.react/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "c69d3c181a0020f3be99",
+    "url": "/calculator.react/static/css/main.38fb9392.chunk.css"
+  },
+  {
+    "revision": "0ad7c35520038a01e2ca00984ad967e3",
+    "url": "/calculator.react/index.html"
+  }
+];
